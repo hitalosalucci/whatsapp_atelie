40ad90a1c46849f59939fc04978bc7d06ee36176
hitalosalucci <hitalo.salucci@hotmail.com>
2021-10-07 21:43:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,1 @@
+window.location.href = "https://api.whatsapp.com/send?phone=5528999214454&text=Ol%C3%A1%2C%20Helen!%20Gostaria%20de%20saber%20mais%20sobre%20as%20pe%C3%A7as%20maravilhosas%20do%20Ateli%C3%AA";
